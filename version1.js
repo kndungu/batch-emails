31878cc2f4537efa0d0100460ee3f635954258ff
Kinuthia Ndung'u <kin.ndungu@gmail.com>
2025-08-03 14:27:29 +0000

    Working

---
+++ version1.js
@@ -0,0 +1,24 @@
+// import samples.json
+import samples from './samples.js';
+
+const buildBatches = () => {
+  const map = {}
+
+  for (const sample of samples) {
+    map[sample.email] = map[sample.email] === undefined ? 0 : map[sample.email] + 1
+  }
+
+  const max = Math.max(...Object.values(map));
+  const batches = Array.from({ length: max + 1 }, () => [])
+
+  for (const sample of samples) {
+    const index = map[sample.email]
+
+    batches[index].push(sample);
+    map[sample.email] = index - 1
+  }
+
+  return batches
+}
+
+console.log(buildBatches(samples).map(batch => batch.length));
---
+++ version2.js
@@ -0,0 +1,69 @@
+// import samples.json
+import samples from './samples.js';
+
+class Queue {
+  constructor(maxIndex) {
+    const indexes = [];
+    
+    for (let i = 0; i <= maxIndex; i++) {
+      indexes.push(i);
+    }
+
+    this.items = indexes;
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  peek() {
+    return this.items[0];
+  }
+
+  rotate() {
+    const item = this.dequeue();
+
+    this.enqueue(item);
+  }
+}
+
+const buildBatches = (samples) => {
+    const map = {}
+
+    for (const sample of samples) {
+      map[sample.email] = map[sample.email] === undefined
+        ? 1
+        : map[sample.email] + 1
+    }
+
+    const max = Math.max(...Object.values(map));
+    const batches = Array.from({ length: max }, () => [])
+
+    const batchIndexesQueue = new Queue(max - 1);
+
+    for (const sample of samples) {
+      let nextBatchIndex = batchIndexesQueue.peek();
+      let nextBatchContainsEmail = batches[nextBatchIndex].some(
+        ({ email }) => email === sample.email
+      )
+
+      while (nextBatchContainsEmail) {
+        batchIndexesQueue.rotate()
+
+        nextBatchIndex = batchIndexesQueue.peek();
+
+        nextBatchContainsEmail = batches[nextBatchIndex].some(({ email }) => email === sample.email );
+      }
+
+      batches[nextBatchIndex].push(sample);
+      batchIndexesQueue.rotate()
+    }
+
+    return batches;
+}
+
+console.log(buildBatches(samples).map(batch => batch.length))
